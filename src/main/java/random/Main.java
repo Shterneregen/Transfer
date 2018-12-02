30a35608796e949b5194693039f9cad2c49bf1ed
shterneregen <shterneregen@gmail.com>
2018-12-02 21:46:08 +0300

    181202; def port

--- src/main/java/random/Main.java
+++ src/main/java/random/Main.java
@@ -2,17 +2,19 @@ package random;
 
 public class Main {
 
-    private static int DEF_PORT = 1988;
+    private static int DEF_PORT = 47372;
 
     public static void main(String[] args) {
         if (args.length > 0) {
             if ("-r".equals(args[0])) {
+                // receiver
                 int port = args.length > 1
                         ? Integer.parseInt(args[1])
                         : DEF_PORT;
                 FileReceiver fs = new FileReceiver(port);
                 fs.start();
             } else if ("-s".equals(args[0])) {
+                // sender
                 if (args.length > 2) {
                     int port = Integer.parseInt(args[1]);
                     String file = args[2];
